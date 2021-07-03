55e1c5abaa744eecbfbf0b5fa2e53ac1eb279ae9
Roman Bebnev <rbebnev@deloitte.com>
2021-07-03 13:54:15 +0500

    some step of test

--- src/views/item.spec.js
+++ src/views/item.spec.js
@@ -1,30 +1,36 @@
 if (typeof define !== 'function') {
   var define = require('amdefine')(module);
 }
-var assert = require('assert');
+const ItemView = require('./item');
+const ItemModel  = require('../models/item');
+const chai = require('chai');
+const expect = chai.expect;
+const assert = require('assert');
+var jsdom = require('mocha-jsdom');
+var rerequire = jsdom.rerequire
+
+jsdom()
 
 const Storage = require('dom-storage');
 global.localStorage = new Storage(null, { strict: true });
 global.sessionStorage = new Storage(null, { strict: true });
 
-var jsdom = require('mocha-jsdom');
-
-global.document = jsdom();
-
-const ItemView = require('./item');
-const ItemModel  = require('../models/item');
-const chai = require('chai');
-const expect = chai.expect;
-
-
 define(function(require) {
 
   describe('Views -> Item', function() {
+
+    var $;
+
+    before(function () {
+      $ = rerequire('jquery')
+    });
+
+    it('works', function () {
+      document.body.innerHTML = '<div>hola</div>';
+      expect($("div").html()).eql('hola');
+    });
+
     it('Item View should be created', function() {
-
-      var $ = require('jquery')(require("jsdom").jsdom().parentWindow);
-
-
       const model = new ItemModel();
       const itemView = new ItemView({ model: model });
       expect(itemView).to.be.ok;
